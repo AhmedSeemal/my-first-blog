d243d5612c040bcdd52ab35008a3900757565024
Ahmed Seemal K <AK00434966@techmahindra.com>
2017-12-29 14:46:50 +0530

    dynamic data fetched upto templates from view logic

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from blog.models import Post
+
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    posts = Post.objects.filter(title = 'sample')
+    return render(request, 'blog/post_list.html', {'posts': posts} )
 
 	
